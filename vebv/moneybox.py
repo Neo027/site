2d8ad737008abda875cfeb5b02523ca99c0a1119
neo <romashkin027@gmail.com>
2023-01-13 18:32:40 +0200

    add moneybox.py file

---
+++ vebv/moneybox.py
@@ -0,0 +1,25 @@
+class MoneyBox:
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def can_add(self, v):
+        return True if v <= self.capacity else False
+
+    def add(self, v):
+        self.capacity -= v
+
+    def print_money(self):
+        print(100 - self.capacity)
+
+m = MoneyBox(100)
+
+while m.capacity != 0:
+    print('Write - "Money", if u want to see your balance')
+    v = input()
+    if v.lower() == 'money':
+        m.print_money()
+    elif m.can_add(int(v)) == True:
+        m.add(int(v))
+    else:
+        print(m.can_add(int(v)))
+print('MoneyBox if full')

5031238350132e5628976b77e1b57acd541baf2a
illia-bilinskyi <illia.bilinskyi@volya.us>
2023-01-13 23:06:51 +0200

    my version of the class

--- vebv/moneybox.py
+++ vebv/moneybox.py
@@ -1,28 +1,26 @@
 class MoneyBox:
     def __init__(self, capacity):
         self.capacity = capacity
+        self.balance = 0
 
     def can_add(self, v):
-        return True if v <= self.capacity else False
+        return True if v <= self.capacity - self.balance else False
 
     def add(self, v):
-        self.capacity -= v
-
-    def print_money(self):
-        print(100 - self.capacity)
-
-m = MoneyBox(100)
-
-while m.capacity != 0:
-    print('Write - "Money", if u want to see your balance')
-    v = input()
-    if v[0] not in '1234567890':
-        if v.lower() == 'money':
-            m.print_money()
+        if self.can_add():
+            self.balance += v
         else:
+            print('insufficient capacity')
+    
+    def input_add(self):
+        try:
+            v = int(input())
+            self.add(v);
+        except ValueError:
             print('incorrect input, try again')
-    elif m.can_add(int(v)) == True:
-        m.add(int(v))
-    else:
-        print(m.can_add(int(v)))
-print('MoneyBox is full')
+
+    def print_balance(self):
+        print(self.balance)
+
+mb = MoneyBox(100)
+mb.input_add()

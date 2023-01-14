f530bc64786b18d46a90c07cb1feba83b2e854c4
neo <romashkin027@gmail.com>
2023-01-14 22:03:17 +0200

    improvement of some functions

--- vebv/moneybox.py
+++ vebv/moneybox.py
@@ -7,20 +7,23 @@ class MoneyBox:
         return True if v <= self.capacity - self.balance else False
 
     def add(self, v):
-        if self.can_add():
+        if self.can_add(v):
             self.balance += v
+            print('Coins in MoneyBox - ', self.balance)
         else:
             print('insufficient capacity')
     
     def input_add(self):
-        try:
-            v = int(input())
-            self.add(v);
-        except ValueError:
-            print('incorrect input, try again')
+        while self.balance != self.capacity:
+            try:
+                v = int(input())
+                self.add(v)
+            except ValueError:
+                print('incorrect input, try again')
 
     def print_balance(self):
         print(self.balance)
 
 mb = MoneyBox(100)
 mb.input_add()
+print('MoneyBox is full!')
